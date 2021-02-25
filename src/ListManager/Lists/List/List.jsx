f99ac93c31b71b5c3f4c12028379a2e4dd8fcf0b
Reden4005 <67143818+Reden4005@users.noreply.github.com>
2021-02-25 15:36:43 +0100

    adding delete lists button (#3)

--- src/ListManager/Lists/List/List.jsx
+++ src/ListManager/Lists/List/List.jsx
@@ -1,16 +1,69 @@
-import React from "react";
-import { Lists } from "../Lists";
+import React, { Component } from "react";
 import classes from "../List/List.module.css";
+import Tasks from "../Tasks/Tasks";
+import InputWithSubmitButton from "../../../UI/InputWithSubmitButton/InputWithSubmitButton";
 
-const list = (props) => {
-    return (
-        <div className={classes.Lists}>
-            {props.lists.map((title, index) => (
-                <Lists  key={index} title={title.listName} lists={props.lists}/> 
-            ))}
-        </div>
-    );
-};
+class List extends Component {
+    state = {
+        listTitle: "",
+        tasks: [],
+        task: "",
+        
+        lists: this.props.lists
 
-export default list;
+    }
+    // componentDidMount() {
+       
 
+    addTaskHandler = (event) => {
+        this.setState({  task: event.target.value, listTitle: this.props.title });
+    }
+
+    confirmAddTaskHandler = (event) => {
+        event.preventDefault();
+        
+        this.setState({
+            tasks: [
+            
+                {
+                    listTitle: this.state.listTitle,
+                    name: this.state.task
+                },
+                ...this.state.tasks
+            ],
+            task: ""
+        });
+        
+        const find = this.props.lists.find(list => list.listName === this.state.listTitle);
+        if (this.state.task) {
+            find.tasks.push(this.state.task);
+        }
+        this.mainInput.value = "";
+    }
+
+    render() {
+        return(
+            <div className={classes.Lists}>
+                <div className={classes.ListHeader}>
+                    <button name={this.props.title} 
+                        onClick={this.props.clicked} 
+                        type="button" 
+                        className={classes.Button + " " + classes.ButtonDelList}>-</button>
+                    <p className={classes.P}>{this.props.title}</p>
+                </div>
+                <Tasks tasks={this.state.tasks} 
+                    key={this.props.title} 
+                    listTitle={this.props.title} 
+                    list={this.state.lists}
+                    deleteList={this.deleteList}/>
+                <InputWithSubmitButton name="task"
+                    placeholder="new task"
+                    changed={this.addTaskHandler}
+                    clear={(ref) => this.mainInput= ref}
+                    clicked={this.confirmAddTaskHandler}/>
+            </div>
+        );
+    }
+}
+
+export { List };
