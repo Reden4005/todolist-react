591ed3d38f3c3b070b715542e2730d7e96e504d6
Reden4005 <olus81_81@o2.pl>
2021-02-26 20:25:41 +0100

    end for now

--- src/UI/Footer/Footer.jsx
+++ src/UI/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import classes from "../Footer/Footer.module.css";
 
 const footer = () => (
     <div className={classes.Footer}>
-        <p className={classes.FooterName}>®2021 Made by Alex </p>
+        <p className={classes.FooterName}>®2021 Made by Alex&nbsp;
+            <a className={classes.FooterEnvelope} href="mailto:olus81_81@o2.pl">✉</a>
+        </p>
         <a className={classes.FooterLink} href="https://pixabay.com/">Images from PIXABAY</a>
     </div>
 );
